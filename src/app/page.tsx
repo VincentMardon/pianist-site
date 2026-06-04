ef3ccfedbc6884a495292619621524c00d2cb9a2
Vincent Mardon <mardon.vincent@gmail.com>
2026-06-04 20:13:47 +0200

    feat(markdown): add basic content navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
-import { page } from '@/styles/global.css';
+import fs from 'node:fs/promises';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
-export default function Home() {
+const pages = {
+  home: 'home.md',
+  presentation: 'presentation.md',
+  tarifs: 'tarifs.md',
+} as const;
+
+type PageKey = keyof typeof pages;
+
+type HomePageProps = {
+  searchParams?: Promise<{
+    page?: string;
+  }>;
+};
+
+export default async function HomePage({ searchParams }: HomePageProps) {
+  const params = await searchParams;
+  const requestedPage = params?.page;
+
+  const currentPage: PageKey = requestedPage === 'presentation' || requestedPage === 'tarifs' ? requestedPage : 'home';
+
+  const markdown = await fs.readFile(`${process.cwd()}/src/content/${pages[currentPage]}`, 'utf-8');
+
   return (
-    <main className={page}>
-      <h1>Pianist Site</h1>
-    </main>
+    <div>
+      <header>
+        <nav>
+          <Link href="/?page=home">Accueil</Link> <Link href="/?page=presentation">Présentation</Link>{' '}
+          <Link href="/?page=tarifs">Tarifs</Link>
+        </nav>
+      </header>
+
+      <main>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </main>
+    </div>
   );
 }
