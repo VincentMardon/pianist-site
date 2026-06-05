dfc9bb44b28fcd51b7e281d396ace808ef7e5314
Vincent Mardon <mardon.vincent@gmail.com>
2026-06-05 08:05:05 +0200

    feat(design-system): add primitive color tokens

---
+++ src/styles/primitiveColors.css.ts
@@ -0,0 +1,16 @@
+export const primitiveColors = {
+  ivory: '#F5F1E8',
+  ebony: '#0B0B0B',
+
+  gold300: '#FEC94D',
+  gold500: '#E1B653',
+  gold700: '#A9854C',
+
+  green300: '#35E708',
+  green500: '#32B512',
+  green700: '#26870D',
+
+  red300: '#F34949',
+  red500: '#DA1D1D',
+  red700: '#AC1616',
+};
