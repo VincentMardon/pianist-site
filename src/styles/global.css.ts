c932f0ab024c939a0f0d4f41623f26fa6191d0ec
Vincent Mardon <mardon.vincent@gmail.com>
2026-06-05 08:44:07 +0200

    style(global-style): add global typography and layout syles

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,5 +1,34 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
+import { semanticColors } from './semanticColors.css';
 
-export const page = style({
+globalStyle('html, body', {
+  margin: 0,
+  padding: 0,
+  minHeight: '100vh',
+  backgroundColor: semanticColors.backgroundPrimary,
+  color: semanticColors.textPrimary,
+});
+
+globalStyle('body', {
+  fontFamily: 'system-ui, sans-serif',
+  lineHeight: 1.6,
+});
+
+globalStyle('main', {
+  maxWidth: '900px',
+  margin: '0 auto',
   padding: '2rem',
+});
+
+globalStyle('h1', {
+  marginBottom: '1.5rem',
+});
+
+globalStyle('h2', {
+  marginTop: '3rem',
+  marginBottom: '1rem',
+});
+
+globalStyle('h1, h2, a', {
+  color: semanticColors.actionPrimary,
 });
