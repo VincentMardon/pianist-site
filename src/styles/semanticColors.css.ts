3c3769bcab755ba12fad38cbe7362b7f67958dd0
Vincent Mardon <mardon.vincent@gmail.com>
2026-06-05 08:15:20 +0200

    feat(design-system): add semantic color system

---
+++ src/styles/semanticColors.css.ts
@@ -0,0 +1,18 @@
+import { primitiveColors } from './primitiveColors.css';
+
+export const semanticColors = {
+  textPrimary: primitiveColors.ivory,
+  backgroundPrimary: primitiveColors.ebony,
+
+  actionPrimaryLight: primitiveColors.gold300,
+  actionPrimary: primitiveColors.gold500,
+  actionPrimaryDark: primitiveColors.gold700,
+
+  successLight: primitiveColors.green300,
+  success: primitiveColors.green500,
+  successDark: primitiveColors.green700,
+
+  dangerLight: primitiveColors.red300,
+  danger: primitiveColors.red500,
+  dangerDark: primitiveColors.red700,
+};
