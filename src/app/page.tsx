25c91479422b8bc91bbce16ccc0493e08c6002d8
Vincent Mardon <mardon.vincent@gmail.com>
2026-06-05 16:13:46 +0200

    fix(page): add h2 tag to a line in home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default async function HomePage({ searchParams }: HomePageProps) {
           <>
             <section>
               <h1>Vincent Mardon</h1>
-              <p>Pianiste accompagnateur</p>
+              <h2>Pianiste accompagnateur</h2>
               <p>
                 J'accompagne chanteurs, instrumentistes et ensembles pour leurs répétitions, auditions, examens,
                 concerts et projets musicaux.
