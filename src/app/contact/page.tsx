682bb74dc2eb15bba40a371dc0275871ba6ef20d
Vincent Mardon <mardon.vincent@gmail.com>
2026-06-22 18:16:11 +0200

    fix: correct typo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import MarkdownPage from '@/components/MarkdownPage';
 export const metadata: Metadata = {
   title: 'Contact | Vincent Mardon',
   description:
-    'Contacter Vincent Mardon pour un accompagnement musical, une répétition, un concert, un cours ou un devsi personnalisé en Île-de-France.',
+    'Contacter Vincent Mardon pour un accompagnement musical, une répétition, un concert, un cours ou un devis personnalisé en Île-de-France.',
 };
 
 export default function ContactPage() {
